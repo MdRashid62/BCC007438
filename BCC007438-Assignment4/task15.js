f5892c7ac11d267238b7e9c71a80067544f1c378
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2019-10-29 14:39:48 +0500

    Update task15.js

--- BCC007438-Assignment4/task15.js
+++ BCC007438-Assignment4/task15.js
@@ -1,12 +1,6 @@
-var day = "10";
-var month = "Feb";
-var year = "1997";
-var dOB = (day + month + year);
-
-var date1 = new Date();
-var date2 = new Date(dOB);
-var age = (date1.getFullYear() - date2.getFullYear());
-var birthYear = (date2.getFullYear());
-
-console.log("Your age is: " + age);
-console.log("Your birth year is: " + birthYear);
+var userInput = prompt("Enter your age to find your birth year:");
+var currDate = new Date();
+var currYear = currDate.getFullYear();
+	
+document.write("Your Age is: " + userInput,"<br/>");
+document.write("Your Birth Year is: " + (currYear - userInput));
